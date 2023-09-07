98685c2d50ac246828c757b64ac75b080346ade3
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-09-06 20:33:55 -0500

    Add validations for lesson
    - Presence validation for all fields except order
    - Url validator for fields. For now they must conform to youtube regular
      and mobile url.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,5 @@
 class Lesson < ApplicationRecord
   belongs_to :instrument
+  validates :name, :instrument_id, presence: true
+  validates :video_url, presence: true, url: { host: /(youtube\.com|youtu\.be)\Z/i }
 end
