b486af23308fb75a26a88ee7a3a4b8da90004fb4
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-09-09 00:09:04 -0500

    Get video id from URL
    Parse the youtube url using the uri package and get the path without the
    leading forward slash

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,12 @@
+require 'uri'
+
 class Lesson < ApplicationRecord
   belongs_to :instrument
   validates :name, :instrument_id, presence: true
   validates :video_url, presence: true, url: { host: /(youtube\.com|youtu\.be)\Z/i }
+
+  def video_id
+    uri = URI.parse(video_url)
+    uri.path[1..uri.path.length]
+  end
 end
--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class LessonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#video_id grabs the video id from the url" do
+    lesson = lessons(:layla)
+    assert_equal lesson.video_id, "something"
+  end
 end
