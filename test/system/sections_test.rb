cd5e83b2eadcd553e5de023a2063c6f8a04d2a4e
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-09-13 02:27:08 -0500

    Add section system tests

--- test/system/sections_test.rb
+++ test/system/sections_test.rb
@@ -2,26 +2,31 @@ require "application_system_test_case"
 
 class SectionsTest < ApplicationSystemTestCase
   setup do
+    @lesson = lessons(:layla)
     @section = sections(:one)
   end
 
   test "visiting the index" do
-    visit sections_url
-    assert_selector "h1", text: "Sections"
+    visit lesson_url(@lesson)
+    assert_selector "h2", text: "Sections"
   end
 
   test "should create section" do
-    visit sections_url
+    visit lesson_url(@lesson)
     click_on "New section"
 
-    check "Current" if @section.current
-    fill_in "End time", with: @section.end_time
-    check "Finished" if @section.finished
-    fill_in "Lesson", with: @section.lesson_id
-    check "Loop" if @section.loop
-    fill_in "Name", with: @section.name
+    fill_in "Name", with: "Part 4"
+    fill_in :section_start_time_hour, with: @section.start_time_hour
+    fill_in :section_start_time_minute, with: @section.start_time_minute
+    fill_in :section_start_time_second, with: @section.start_time_second
+    fill_in :section_end_time_hour, with: @section.end_time_hour
+    fill_in :section_end_time_minute, with: @section.end_time_minute
+    fill_in :section_end_time_second, with: @section.end_time_second
+    check "Finished"
+    check "Loop"
     fill_in "Playback speed", with: @section.playback_speed
-    fill_in "Start time", with: @section.start_time
+    check "Current" if @section.current
+
     click_on "Create Section"
 
     assert_text "Section was successfully created"
@@ -29,27 +34,40 @@ class SectionsTest < ApplicationSystemTestCase
   end
 
   test "should update Section" do
-    visit section_url(@section)
-    click_on "Edit this section", match: :first
+    visit lesson_url(@lesson)
 
-    check "Current" if @section.current
-    fill_in "End time", with: @section.end_time
-    check "Finished" if @section.finished
-    fill_in "Lesson", with: @section.lesson_id
-    check "Loop" if @section.loop
-    fill_in "Name", with: @section.name
-    fill_in "Playback speed", with: @section.playback_speed
-    fill_in "Start time", with: @section.start_time
+    assert_selector "summary", text: "Intro"
+
+    find("details", text: "Intro").click
+    # click_on "details"
+    click_on "Edit section", match: :first
+
+    fill_in "Name", with: "Part 3"
+    fill_in :section_start_time_hour, with: (@section.start_time_hour)
+    fill_in :section_start_time_minute, with: (@section.start_time_minute + 2)
+    fill_in :section_start_time_second, with: (@section.start_time_second + 2)
+    fill_in :section_end_time_hour, with: (@section.end_time_hour)
+    fill_in :section_end_time_minute, with: (@section.end_time_minute + 2)
+    fill_in :section_end_time_second, with: (@section.end_time_second + 2)
+    uncheck "Finished" if @section.finished
+    uncheck "Loop" if @section.loop
+    fill_in "Playback speed", with: 2.5
+    uncheck "Current"
+
     click_on "Update Section"
 
     assert_text "Section was successfully updated"
     click_on "Back"
   end
 
-  test "should destroy Section" do
-    visit section_url(@section)
-    click_on "Destroy this section", match: :first
+   test "should destroy Section" do
+     visit lesson_url(@lesson)
 
-    assert_text "Section was successfully destroyed"
-  end
+     find("details", text: "Intro").click
+     click_on "Destroy this section", match: :first
+
+     assert_text "Layla"
+     assert_selector "h2", text: "Sections"
+     assert_text "Section was successfully destroyed"
+   end
 end
