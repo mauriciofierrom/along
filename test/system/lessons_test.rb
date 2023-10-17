1f1336883fe76106b0fe9f259342ac40357096e7
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-10-17 15:25:10 -0500

    Fix lesson system tests

--- test/system/lessons_test.rb
+++ test/system/lessons_test.rb
@@ -7,7 +7,7 @@ class LessonsTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit lessons_url
-    assert_selector "h1", text: "Lessons"
+    assert_selector ".lesson-title", text: "Lessons"
   end
 
   test "should create lesson" do
@@ -17,10 +17,11 @@ class LessonsTest < ApplicationSystemTestCase
     select "Guitar", from: "Instrument"
     fill_in "Name", with: "Wild Blue"
     fill_in "Video URL", with: "https://youtube.com/something2"
+    page.execute_script("document.querySelector('#lesson_duration_in_seconds').value = 123");
+
     click_on "Create Lesson"
 
-    assert_text "Lesson was successfully created"
-    click_on "Back"
+    assert_text "Wild Blue"
   end
 
   test "should update Lesson" do
@@ -32,10 +33,10 @@ class LessonsTest < ApplicationSystemTestCase
     fill_in "Video URL", with: "https://youtube.com/other-thing"
     click_on "Update Lesson"
 
-    assert_text "Lesson was successfully updated"
+    # assert_text "Lesson was successfully updated"
     click_on "Back to lessons"
 
-    assert_selector "h1", text: "Lessons"
+    assert_selector ".lesson-title", text: "Lessons"
     assert_text "New Light"
   end
 
