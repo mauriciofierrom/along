e07e9b7b14e0846592ba646536fa4fe8ebe5163d
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-11-16 13:20:51 -0500

    Add helper to show section start/end
    If the start/end is less than an hour show a shorter version that
    includes only the minutes and the seconds.

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,14 +1,32 @@
 module SectionsHelper
   def format_start_time(section)
-    "#{section.start_time_hour.to_s.rjust(2, "0")}:#{section.start_time_minute.to_s.rjust(2, "0")}:#{section.start_time_second.to_s.rjust(2, "0")}"
+    "#{pad_time(section.start_time_hour)}:#{pad_time(section.start_time_minute)}:#{pad_time(section.start_time_second)}"
   end
 
   def format_end_time(section)
-    "#{section.end_time_hour.to_s.rjust(2, "0")}:#{section.end_time_minute.to_s.rjust(2, "0")}:#{section.end_time_second.to_s.rjust(2, "0")}"
+    "#{pad_time(section.end_time_hour)}:#{pad_time(section.end_time_minute)}:#{pad_time(section.end_time_second)}"
+  end
+
+  def format_time(time)
+    return time.to_s if time.hour > 0
+    "#{pad_time(time.minute)}:#{pad_time(time.second)}"
   end
 
   def format_playback_speed(speed)
     return speed.to_i if speed % 1 == 0.0
     speed
+  end
+
+  def loop_img(section)
+    return "repeat.svg" if section.loop
+    "once.png"
+  end
+
+  private
+
+  def pad_time(time)
+    time
+      .to_s
+      .rjust(2, "0")
   end
 end
