25ae6e4bd43cf64778eeff4cd0d2a58df9cad3da
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-01 21:10:06 -0500

    Redirect to lesson#index on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
+  def after_sign_in_path_for(users)
+    lessons_path
+  end
 end
