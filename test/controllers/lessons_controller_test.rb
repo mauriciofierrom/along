ee864e81443f9b0b0ae5b87b22e2214a375fd2c9
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-01 23:24:04 -0500

    Update controller tests
    - Include Devise helpers
    - Sign in to the default user

--- test/controllers/lessons_controller_test.rb
+++ test/controllers/lessons_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class LessonsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    sign_in users(:default)
     @lesson = lessons(:layla)
   end
 
