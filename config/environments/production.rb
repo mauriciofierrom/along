608ca5b46e5c85b6ddc88033d412e8420b002cc0
Mauricio Fierro <mauriciofierrom@gmail.com>
2023-12-13 18:52:13 -0500

    Set fly as the domain for now in production
    To enable action mailer to work properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'along.fly.dev' }
 
   config.action_mailer.smtp_settings = {
       :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
