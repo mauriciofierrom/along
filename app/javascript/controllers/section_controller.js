b282e8edefe668fd7348ebb791644b7ac86f7b08
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-04-07 16:00:52 -0500

    Add screen lock manager to section controller
    - On connection try to acquire the lock. The connection is performed via
      turbo streams which respond to a click, which seems to be enough to
      mark the user intent
    - On disconnection we release the lock if it was acquired

--- app/javascript/controllers/section_controller.js
+++ app/javascript/controllers/section_controller.js
@@ -1,17 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
+import ScreenLockManager from "controllers/screen_lock_manager"
 
 export default class extends Controller {
+  #screenLockManager;
+
+  initialize () {
+    console.log("initialize section controller")
+    this.#screenLockManager = new ScreenLockManager()
+  }
   connect() {
+    console.log("connected section controller")
     console.log(`Section load start: ${this.element.dataset.start}`)
     console.log(`Section load end: ${this.element.dataset.end}`)
     const start = parseFloat(this.element.dataset.start)
     const end = parseFloat(this.element.dataset.end)
 
     this.dispatch("connect", { detail: {start: start, end: end}})
+    this.#screenLockManager.acquireScreenLock()
   }
 
   disconnect() {
-    console.log("section disconnect")
+    console.log("section controller disconnect")
     this.dispatch("disconnect")
+    this.#screenLockManager.releaseLock()
   }
 }
