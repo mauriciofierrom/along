4d32ce585f5855854d8402e577ae344d0e4edb35
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-06-19 08:34:38 -0500

    Update lesson tests

--- test/system/lessons_test.rb
+++ test/system/lessons_test.rb
@@ -13,11 +13,11 @@ class LessonsTest < ApplicationSystemTestCase
 
   test "should create lesson" do
     visit lessons_url
-    click_on "New lesson"
+    click_on "New Lesson"
 
     select "Guitar", from: "Instrument"
     fill_in "Name", with: "Wild Blue"
-    fill_in "Video URL", with: "https://youtube.com/something2"
+    fill_in "Video link", with: "https://youtube.com/something2"
     page.execute_script("document.querySelector('#lesson_duration_in_seconds').value = 123");
 
     click_on "Create Lesson"
@@ -31,7 +31,7 @@ class LessonsTest < ApplicationSystemTestCase
 
     select "Guitar", from: "Instrument"
     fill_in "Name", with: "New Light"
-    fill_in "Video URL", with: "https://youtube.com/other-thing"
+    fill_in "Video link", with: "https://youtube.com/other-thing"
     click_on "Update Lesson"
 
     # assert_text "Lesson was successfully updated"
