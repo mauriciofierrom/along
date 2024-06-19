154a0bf1cd6c5a807d467d3a09ccc4b97a8a31c0
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-06-19 08:18:46 -0500

    Configure i18n
    - Add gem 'http_accept_language' gem to determine locale based on the
      Accept-Language HTTP header
    - Configure paths to locale files
    - Set available locales. We initially support english and spanish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  include HttpAcceptLanguage::AutoLocale
   before_action :authenticate_user!, unless: :devise_controller?
+
   def after_sign_in_path_for(users)
     lessons_path
   end
