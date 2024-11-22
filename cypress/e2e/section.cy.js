bfd618a9a3674acbba16d69295c8289783153bee
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-11-22 03:41:20 -0500

    Add testing-library/cypress

--- cypress/e2e/section.cy.js
+++ cypress/e2e/section.cy.js
@@ -1,14 +1,11 @@
 describe("Section", () => {
   it("Loads the section", () => {
-    let sectionName
-
     cy.appFactories([["create", "section"]]).then(([section]) => {
       console.log(section)
-      sectionName = section.name
 
       cy.forceLogin({ redirect_to: "/lessons"})
       cy.get(".video-card > .title").click()
-      cy.contains(sectionName)
+      cy.findByText(section.name).should('exist')
     })
   })
 })
