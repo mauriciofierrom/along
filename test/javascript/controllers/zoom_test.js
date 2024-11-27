ba4484744754f42019dea93eac31d4822b77aaf2
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-11-27 06:02:17 -0500

    Add zoom convertion tests
    Rebase with the zoom

---
+++ test/javascript/controllers/zoom_test.js
@@ -0,0 +1,28 @@
+import Zoom from "../../../app/javascript/controllers/zoom"
+
+describe("Zoom", () => {
+  it("converts a point", () => {
+    const zoom = new Zoom(0, 72, 1273)
+    const { start, end } = zoom.convert(323.28, 535.1)
+
+    expect(parseFloat(start.toFixed(2))).toBe(18.28)
+    expect(parseFloat(end.toFixed(2))).toBe(30.26)
+  })
+
+  it("reverts a point", () => {
+    const zoom = new Zoom(0.0, 72.0, 1273)
+
+    // TODO: Not exact conversion
+    expect(parseFloat(zoom.restorePoint(18.28).toFixed(2))).toBe(323.2)
+    expect(parseFloat(zoom.restorePoint(30.26).toFixed(2))).toBe(535.01)
+  })
+
+  it("is reversible", () => {
+    const zoom = new Zoom(0,72, 1273)
+    const point = 323.28
+    const converted = zoom.convertPoint(point)
+    const reverted = zoom.restorePoint(converted)
+
+    expect(point).toBe(reverted)
+  })
+})
