0bd02127a1541e515f26bb45025730adbfea3174
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-11-27 05:39:32 -0500

    Add zoom field stimulus controller
    This controller is in charge of managing the nested fields for Zoom in
    Section's form.
    
    - Adds the click event listener to zoom out since the structure of
      elements prevent using regular events unless using window, which could
      be refactores later.
    - Reacts to removal of fields and triggers events to respond to this
      changes in other controllers
    - Adds a destroy input when we're editing a section and it already
      contains zoom fields that should be marked for removal on form
      submission. Dynamic/non-persisted fields are just removed from DOM
      since they aren't persisted

---
+++ app/javascript/controllers/zoom_field_controller.js
@@ -0,0 +1,83 @@
+import { Controller } from "@hotwired/stimulus";
+import { debug } from "controllers/util";
+
+export default class extends Controller {
+  static targets = [ "zoomField", "zoomDestroy" ]
+
+  initialize() {
+    console.log("init zoom field controller")
+  }
+
+  connect() {
+    const zoomOutButton = document.querySelector("#zoom-out")
+    zoomOutButton.addEventListener("click", (_e) => {
+      debug("zoom out click")
+      if(this.hasZoomFieldTarget) {
+        debug("has zoom field")
+        let nonRemovedTargets = this.zoomFieldTargets.filter(t =>
+          t.querySelector("input[name$='[_destroy]']") === null
+        )
+
+        if(nonRemovedTargets.length > 0) {
+          let toRemove = nonRemovedTargets[nonRemovedTargets.length - 1]
+          const isPersisted = toRemove.querySelector("input[id$='id']")
+
+          if(isPersisted !== null) {
+            debug("Is persisted")
+            const nameAttr = toRemove.querySelector('input').name
+            const index = nameAttr.match(/\[(\d+)\]/)[1]
+            const destroyInput = this.#buildDestroyInput(index)
+            toRemove.appendChild(destroyInput)
+          } else {
+            toRemove.remove()
+          }
+        }
+      }
+    })
+  }
+
+  zoomFieldTargetConnected(el) {
+    debug("zoom field connected?")
+
+    if(el.dataset.existing !== "true") {
+      const start = parseFloat(el.querySelector("input[id$='start']").value)
+      const end = parseFloat(el.querySelector("input[id$='end']").value)
+
+      debug(`Start: ${start}. End: ${end}`)
+
+      this.dispatch("addZoomLevel", { detail: { start, end } })
+    }
+  }
+
+  /*
+   * A zoom-field target is removed from the DOM when it's a non-persisted
+   * entry. That means we added the item to be saved on form submit, but decided
+   * to remove it now.
+   */
+  zoomFieldTargetDisconnected(_el) {
+    debug("Zoom field target disconnected")
+    this.dispatch("removeZoomLevel")
+    this.dispatch("removeZoomIndicator")
+  }
+
+  /*
+   * We also trigger the removal of a zoom events for persisted zoom fields,
+   * which means that we react to the destroy hidden input being added to the
+   * fields in the form to mark the destruction of the zoom record on submit
+   */
+  zoomDestroyTargetConnected(_el) {
+    debug("zoom destroy connected")
+    this.dispatch("removeZoomLevel")
+    this.dispatch("removeZoomIndicator")
+  }
+
+  #buildDestroyInput(index){
+    const destroyInput = document.createElement("input")
+    destroyInput.type = "hidden"
+    destroyInput.name = `section[zoom_attributes][${index}][_destroy]`
+    destroyInput.value = "1"
+    destroyInput.dataset.zoomFieldTarget = "zoomDestroy"
+
+    return destroyInput
+  }
+}
