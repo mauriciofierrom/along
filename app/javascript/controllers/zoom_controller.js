542c77b8b5a83ad835bd8c69d5fc623f88e96c04
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-11-27 05:32:11 -0500

    Add Zoom controller
    This controller is in charge of managing state of zoom controls like the
    zoom in/out buttons.
    
    - Keeps the zoom-in form up to date when points
    change
    - Triggers properties of hidden/disable according to the events
      dispatched from other controllers and initial states

---
+++ app/javascript/controllers/zoom_controller.js
@@ -0,0 +1,122 @@
+import { Controller } from "@hotwired/stimulus";
+import { debug, show, hide, disable, enable } from "controllers/util";
+
+/** Controller for zoom actions */
+export default class extends Controller {
+  static targets = [ "zoomIn", "zoomOut", "zoomStart", "zoomEnd", "zoomDuration" ]
+
+  /**
+   * Sets up the initial state when a zoom level exists
+   */
+  ready({detail: { duration, currentZoomLevel, isEdit }}) {
+    this.#setDuration(duration)
+
+    if(isEdit) {
+      show(this.zoomInTarget)
+    }
+
+    if(currentZoomLevel > 0) {
+      show(this.zoomOutTarget)
+    }
+  }
+
+  /*
+   * Actions when we finish with section edition
+   */
+  zoomCancelled(_) {
+    debug("cancelled")
+    hide(this.zoomInTarget)
+    hide(this.zoomOutTarget)
+    this.#clear()
+  }
+
+  /*
+   * Set the current range selection as the potential zoom
+   */
+  setPoint(start, end) {
+    console.log("Setting point", start, end)
+    this.zoomStartTarget.value = start
+    this.zoomEndTarget.value = end
+
+    // TODO: There might be an issue with the rangeUpdated function
+    if(start !== 0 || end !== max) {
+      enable(this.zoomInTarget)
+      show(this.zoomInTarget)
+    }
+  }
+
+  /*
+   * Event when a zoom level has been added
+   *
+   * The first time this happens we show the button for zooming out (basically
+   * to allow undoing zooming in. We also disable the zoom-in button because
+   * when we add a new zoom, we reset the range's values to 0 - duration, thus
+   * it would be redundant to zoom-into the already zoomed-into portion of the
+   * video.
+   */
+  zoomLevelAdded({detail: { zoomLevel }}) {
+    debug("zoom level added")
+    if(zoomLevel === 1) {
+      show(this.zoomOutTarget)
+    }
+    disable(this.zoomInTarget)
+  }
+
+  /*
+   * Event when a zoom level has been removed
+   *
+   * We have a maximum of 3 zoom levels and we have to cover what happens when
+   * we zoomed-out of all of them. On section creation we default to the
+   * original duration of the video. On section edition we fallback to the
+   * original section's value
+   */
+  zoomLevelRemoved({ detail: { zoomLevel, isEdit } }) {
+    debug(`ZoomLevel: ${zoomLevel}. isEdit: ${isEdit}`)
+    if(zoomLevel > 0 && zoomLevel < 3) {
+      show(this.zoomOutTarget)
+    }
+
+    if(zoomLevel === 0) {
+      hide(this.zoomOutTarget)
+      if(isEdit) {
+        show(this.zoomInTarget)
+        enable(this.zoomInTarget)
+      } else {
+        disable(this.zoomInTarget)
+      }
+    }
+  }
+
+  /*
+   * Event fired when the range has been updated
+   */
+  rangeUpdated({ detail: { start, end, max }}) {
+    debug(`Start: ${start}. End: ${end}. Max: ${max}`)
+    if(start !== 0 || end !== max) {
+      enable(this.zoomInTarget)
+      show(this.zoomInTarget)
+    }
+    // TODO: Prime candidate to override the use of the zoomOutlet in
+    // PlayerController.
+  }
+
+  /*
+   * The duration of the video to calculate various
+   * zoom related translations. To be called via dispatch
+   *
+   * @param {number} duration - The duration in seconds
+   */
+  #setDuration(duration) {
+    if(this.hasZoomDurationTarget) {
+      this.zoomDurationTarget.value = duration
+    }
+  }
+
+  /*
+   * Clear the zoom-in form's values
+   */
+  #clear() {
+    this.zoomStartTarget.value = ""
+    this.zoomEndTarget.value = ""
+  }
+}
