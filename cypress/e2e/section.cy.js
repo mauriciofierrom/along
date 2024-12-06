aed2c93783a0c9a1f5c97e437e5e135b28220f8e
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-12-05 20:17:44 -0500

    Add specs

--- cypress/e2e/section.cy.js
+++ cypress/e2e/section.cy.js
@@ -1,8 +1,6 @@
 describe("Section", () => {
   it("Loads the section", () => {
     cy.appFactories([["create", "section"]]).then(([section]) => {
-      console.log(section)
-
       cy.forceLogin({ redirect_to: "/lessons"})
       cy.get(".video-card > .title").click()
       cy.findByText(section.name).should('exist')
