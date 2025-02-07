45c8a666d9c2fbe4fd9b22285162fa7daba53051
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-02-07 06:58:50 -0500

    WIP

--- test/helpers/lessons_helper_test.rb
+++ test/helpers/lessons_helper_test.rb
@@ -4,7 +4,6 @@ class LessonsHelperTest < ActionView::TestCase
   include LessonsHelper
 
   test "#video_id grabs the video id from the url" do
-
     lesson = lessons(:layla)
     assert_equal video_id(lesson), "something"
   end
