57ca0917f8df30529f3d0e07039de6a6387e6aeb
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-04-02 16:00:37 -0500

    Pick default opposite value
    When first picking a point we automatically pick the opposite point
    based on 10% of the duration of the video. The criteria for "first" is:
    
    - When adding a new section
    - When zooming in (which resets the points to full-duration)
    - When zooming out (which resets the points to full-duration)

--- app/javascript/controllers/zoom_in_controller.js
+++ app/javascript/controllers/zoom_in_controller.js
@@ -1,24 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
-import { debug } from "controllers/util"
 
 export default class extends Controller {
   static targets = ["start", "end"]
   static outlets = ["zoom"]
 
   /*
-   * Set the current range selection as the potential zoom
+   * Set the current range selection as the potential zoom on initial range
+   * input load
    */
-  rangeInputUpdated({ detail: { start, end } }) {
+  rangeInputReady({ detail: { start, end } }) {
     this.startTarget.value = start
     this.endTarget.value = end
   }
 
   /*
-   * Set the current range selection as the potential zoom on initial range
-   * input load
+   * Set the current range selection as the potential zoom
    */
-  rangeInputReady({ detail: { start, end } }) {
-    debug("range input ready with", { start, end })
+  rangeInputUpdated({ detail: { start, end } }) {
     this.startTarget.value = start
     this.endTarget.value = end
   }
