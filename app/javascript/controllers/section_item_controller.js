0bebf9135a43caf23da9d7a044c90fdda9fdafb4
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-04-03 20:19:57 -0500

    Add Section Item Stimulus controller
    Add a Section Item stimulus controller that is in charge of handling the
    events of the Drag and Drop API.
    
    - Augment the Section Item markup with the section id, the drag and drop
      events and attributes,  and the section-item controller. Section Items
      act as both draggable elements and drop zones so need to have the
      draggable attribute and the required handlers to mark them as drop
      zones.
      - Add the Section Item stimulus controller. It's in charge of handling
      the Drag and Drop events and performing the request to the backend to
      perform the persistance of the swap. If the request succeeds it also
      swaps the elements in the DOM.

---
+++ app/javascript/controllers/section_item_controller.js
@@ -0,0 +1,92 @@
+import { Controller } from "@hotwired/stimulus"
+import { debug } from "controllers/util"
+
+/**
+ * Controller for Section Items
+ *
+ * - Drag and drop
+ *
+ *   Every Section item is both a draggable element and a drop zone, which
+ *   allows us to swap them in place
+ */
+export default class extends Controller {
+  /**
+   * The handler for the section item's dragstart event
+   *
+   * We set the data to the section id of the section item being dragged to be
+   * able to fetch it later in the drop event of the receiving section item by
+   * using the element's dataset
+   */
+  dragStart(event) {
+    debug("starting?")
+    event.dataTransfer.setData(
+      "application/section-id",
+      event.target.dataset.sectionId,
+    )
+    event.dataTransfer.dropEffect = "move"
+  }
+
+  /**
+   * The handler for the section item's `drop` event
+   *
+   * We retrieve the dragged item Section's id from the data set in `dragstart`
+   * and the dropped-into item Section's id from the event's current target and
+   * use both ids as payload to an endpoint to perform the swap of the orders.
+   *
+   * If it succeeds, both elements are swaped in place visually as well.
+   */
+  onDrop(event) {
+    debug("dropped?", event.target)
+    const draggedSectionItemId = parseInt(
+      event.dataTransfer.getData("application/section-id"),
+      10,
+    )
+    const droppedSectionItemId = parseInt(
+      event.currentTarget.dataset.sectionId,
+      10,
+    )
+
+    const droppedElement = event.currentTarget
+
+    fetch(`/section/swap_order/`, {
+      method: "POST",
+      body: JSON.stringify({
+        dragged_id: draggedSectionItemId,
+        dropped_id: droppedSectionItemId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+          .content,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error("It failed. What a drag!")
+        const draggedElement = document.querySelector(
+          `[data-section-id="${draggedSectionItemId}"]`,
+        )
+        this.#swap(draggedElement, droppedElement)
+      })
+      .catch((error) => console.error(error))
+  }
+
+  onDragOver(event) {
+    debug("dragged over?", event.target)
+  }
+
+  onDragEnd() {
+    debug("finished?")
+  }
+
+  #swap(sectionItem, otherSectionItem) {
+    const firstSection = sectionItem.parentNode
+    const secondSection = otherSectionItem.parentNode
+
+    const parent = firstSection.parentNode
+    const next = firstSection.nextSibling
+    const otherNext = secondSection.nextSibling
+
+    parent.insertBefore(firstSection, otherNext)
+    parent.insertBefore(secondSection, next)
+  }
+}
