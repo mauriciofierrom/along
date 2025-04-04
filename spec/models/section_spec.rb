1fc6637d7be7a850c7268ae690fc966d30fdc908
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-04-03 20:28:19 -0500

    Update the Section model specs
    - Check the order's uniqueness validation and scope
    - Check that the set_order callback is fired
    - Check the behavior of the set_order callback

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -9,5 +9,26 @@ describe Section, type: :model do
 
   describe 'validations' do
     it { should validate_uniqueness_of(:name).scoped_to(:lesson_id) }
+    it { should validate_uniqueness_of(:order).scoped_to(:lesson_id) }
+  end
+
+  describe '#save' do
+    describe "#set_order" do
+      let(:new_section) { build(:section, lesson: subject.lesson) }
+
+      it "triggers set_order on save" do
+        expect(new_section).to receive(:set_order)
+        new_section.save!
+      end
+
+      it "should set the order to max value per lesson plus one" do
+        expect(subject.order).to eq(1)
+        expect(new_section.order).to be_nil
+
+        new_section.save!
+
+        expect(new_section.order).to eq(2)
+      end
+    end
   end
 end
