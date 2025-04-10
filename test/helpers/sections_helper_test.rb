9fe95584776a3b042fa144a2e3ef63e3c617688c
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-04-10 14:58:19 -0500

    Add invalid styles to Lesson validation
    Add the invalid styles to lesson fields when reporting the list of
    errors in a traditional Rails style

--- test/helpers/sections_helper_test.rb
+++ test/helpers/sections_helper_test.rb
@@ -34,4 +34,18 @@ class SectionsHelperTest < ActionView::TestCase
     section = sections(:two)
     assert_equal loop_img(section), PLAY_ONCE_IMG
   end
+
+  test ".error_style is 'invalid' when the field is present in the error hash" do
+    errors = { some: "error" }
+    assert_equal error_style(:some, errors), "invalid"
+  end
+
+  test ".error_style is empty when the error hash is nil" do
+    assert_equal error_style(:some, nil), ""
+  end
+
+  test ".error_style is empty when the field is not present in the error hash" do
+    errors = { other: "error" }
+    assert_equal error_style(:some, errors), ""
+  end
 end
