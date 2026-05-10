f036aa9f7e7d7bd65bb1014d736234d4fceae4ad
Mauricio Fierro <mauriciofierrom@gmail.com>
2026-05-10 00:57:44 -0500

    Update eslint configuration
    - Make eslint configuration use jsconfig
      In order to support the imports as configured
    - Add setImmediate to globals
    - Add preload file to ignores
    - Add bun:test to core-modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,25 @@
-import { fixupConfigRules } from "@eslint/compat";
-import shopifyEslintPlugin from '@shopify/eslint-plugin';
-import pluginJest from 'eslint-plugin-jest';
-import globals from 'globals';
-import pluginCypress from 'eslint-plugin-cypress/flat';
+import { fixupConfigRules } from "@eslint/compat"
+import shopifyEslintPlugin from "@shopify/eslint-plugin"
+import pluginJest from "eslint-plugin-jest"
+import globals from "globals"
+import pluginCypress from "eslint-plugin-cypress/flat"
 
 export default [
   ...fixupConfigRules(shopifyEslintPlugin.configs.esnext),
   ...fixupConfigRules(shopifyEslintPlugin.configs.prettier),
+  {
+    ignores: ["**/preload.ts"],
+  },
   pluginCypress.configs.globals,
   {
-    ignores: ["**/setup.js", "eslint.config.mjs", "**/cypress/support/", "cypress.config.js", "**/controllers/index.js", "**/application.js"],
+    ignores: [
+      "**/setup.js",
+      "eslint.config.mjs",
+      "**/cypress/support/",
+      "cypress.config.js",
+      "**/controllers/index.js",
+      "**/application.js",
+    ],
   },
   {
     files: ["app/javascript/controllers/**/*.js"],
@@ -20,42 +30,53 @@ export default [
     languageOptions: {
       globals: {
         ...globals.browser,
-      }
-    }
+      },
+    },
   },
   {
     rules: {
       "prettier/prettier": "off",
-      "no-console": ["error", { "allow": ["error"]}]
-    }
+      "no-console": ["error", { allow: ["error"] }],
+    },
   },
   {
     // update this to match your test files
-    files: ['test/javascript/**/*_test.js'],
+    files: ["test/javascript/**/*_test.js"],
     plugins: { jest: pluginJest },
     languageOptions: {
       globals: {
         ...pluginJest.environments.globals.globals,
         ...globals.browser,
+        setImmediate: "readonly",
       },
     },
     rules: {
-      'jest/no-disabled-tests': 'warn',
-      'jest/no-focused-tests': 'error',
-      'jest/no-identical-title': 'error',
-      'jest/prefer-to-have-length': 'warn',
-      'jest/valid-expect': 'error',
-      'no-warning-comments': 'off',
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+      "no-warning-comments": "off",
     },
   },
   {
-    files: ['cypress/e2e/**/*.cy.js'],
+    files: ["cypress/e2e/**/*.cy.js"],
     plugins: { cypresss: pluginCypress },
     rules: {
       ...fixupConfigRules(pluginCypress.configs.recommended).rules,
-      'promise/catch-or-return': 'off',
-      'promise/no-nesting': 'off',
-      'no-warning-comments': 'off',
-    }
-  }
-];
+      "promise/catch-or-return": "off",
+      "promise/no-nesting": "off",
+      "no-warning-comments": "off",
+    },
+  },
+  {
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./jsconfig.json",
+        },
+      },
+      "import/core-modules": ["bun:test"],
+    },
+  },
+]
