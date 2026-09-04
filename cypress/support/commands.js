1f87e69bd8addfceb471fb56094afd9bf3421bd7
Mauricio Fierro <mauriciofierrom@gmail.com>
2026-09-04 17:43:05 -0500

    Add shouldBeEnabled custom command
    Elements can be disabled by both the disabled class and the disabled
    attribute so we add a custom Cypress command to check both.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,4 +23,13 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-import '@testing-library/cypress/add-commands'
+import "@testing-library/cypress/add-commands"
+
+Cypress.Commands.add(
+  "shouldBeEnabled",
+  { prevSubject: "element" },
+  (subject) => {
+    cy.wrap(subject).should("not.have.class", "disabled").and("not.be.disabled")
+  },
+)
+
